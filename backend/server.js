4b3c6cc7f88a9640d6544c6c3dc117608d8c3149
singhkunaraman <singhkumaraman45@gmail.com>
2024-01-24 19:47:55 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
 var cors = require("cors");
+var PORT = process.env.PORT || 5000;
 app.use(cors());
 connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api/expense", require("./routes/expenseRoutes"));
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log("Server Running ");
 });
