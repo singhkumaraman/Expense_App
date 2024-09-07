3fd54811b07bdccc36f766e56cf3e5f6d78871a3
Aman Kumar Singh <101496965+singhkumaraman@users.noreply.github.com>
2024-09-07 16:58:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api/expense", require("./routes/expenseRoutes"));
-console.log(process.env.PORT);
-app.listen(process.env.PORT, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log("Server Running ");
 });
