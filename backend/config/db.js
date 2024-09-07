b410c5e3c5eb459aacaaf09f4623377d19c976ac
singhkunaraman <singhkumaraman45@gmail.com>
2024-09-07 12:41:06 +0530

    vercel.json-file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  // console.log(process.env.MONGO_URI);
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("Database Connected...");
+    console.log("Database Connected");
   } catch (error) {
-    console.error("Error connecting to the database:", error.message);
+    console.error(error.message);
     process.exit(1);
   }
 };
